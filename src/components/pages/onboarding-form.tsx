3e3880272937a3472aebafc201dd6cf1b6f2d8c9
Nguyen Nguyen <nguyenkdksnb@gmail.com>
2024-04-22 22:48:32 +0700

    feat: add select location to onboarding

--- src/components/pages/onboarding-form.tsx
+++ src/components/pages/onboarding-form.tsx
@@ -16,6 +16,9 @@ import { Input } from "@/components/ui/input";
 import { phoneRegex } from "@/lib/utils";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import ChooseLocation from "@/app/location/locationselector";
+import useLocationForm from "@/app/location/useLocationForm";
+import Select from "react-select";
 
 const formSchema = z.object({
   firstName: z.string().min(1, {
@@ -55,6 +58,21 @@ function OnboardingForm() {
     },
     resolver: zodResolver(formSchema),
   });
+  const { state, onCitySelect, onDistrictSelect, onWardSelect } = useLocationForm(false);
+  const [isDefault, setIsDefault] = useState(false);
+  const {
+    cityOptions,
+    districtOptions,
+    wardOptions,
+    selectedCity,
+    selectedDistrict,
+    selectedWard,
+  } = state;
+  const [disable, setDisable] = useState(true);
+  const [city, setCity] = useState("");
+  const [district, setDistrict] = useState("");
+  const [ward, setWard] = useState("");
+  const [idAddress, setIdAddress] = useState("");
 
   const onSubmit = handleSubmit(async (data) => {
     setIsSubmit(true);
@@ -65,7 +83,7 @@ function OnboardingForm() {
       <CardHeader>
         <CardTitle className="text-xl">Sign Up</CardTitle>
         <CardDescription>
-          Enter your information to create an account
+          Enter your information to complete verification
         </CardDescription>
       </CardHeader>
       <CardContent>
@@ -99,6 +117,64 @@ function OnboardingForm() {
                 </p>
               )}
             </div>
+          </div>
+          <div className="grid gap-2">
+          <Select
+                      name="cityId"
+                      className="my-react-select-container"
+                      classNamePrefix="my-react-select"
+                      key={`cityId_${selectedCity?.value}`}
+                      isDisabled={cityOptions.length === 0 || isDefault}
+                      options={cityOptions}
+                      onChange={(option) => {
+                        if (option) {
+                          onCitySelect({
+                            value: option.value,
+                            label: option.label,
+                          });
+                        }
+                      }}
+                      placeholder="Tỉnh/Thành"
+                      defaultValue={selectedCity}
+                    />
+
+                    <Select
+                      name="districtId"
+                      key={`districtId_${selectedDistrict?.value}`}
+                      isDisabled={districtOptions.length === 0}
+                      options={districtOptions}
+                      onChange={(option) => {
+                        if (option)
+                          onDistrictSelect({
+                            value: option.value,
+                            label: option.label,
+                          });
+                      }}
+                      placeholder="Quận/Huyện"
+                      className="z-5 my-react-select-container"
+                      classNamePrefix="my-react-select"
+                      defaultValue={selectedDistrict}
+                    />
+
+                    <Select
+                      name="wardId"
+                      key={`wardId_${selectedWard?.value}`}
+                      isDisabled={wardOptions.length === 0}
+                      options={wardOptions}
+                      placeholder="Phường/Xã"
+                      className="z-5 my-react-select-container"
+                      classNamePrefix="my-react-select"
+                      onChange={(option) => {
+                        if (option) {
+                          setDisable(false);
+                          onWardSelect({
+                            value: option.value,
+                            label: option.label,
+                          });
+                        }
+                      }}
+                      defaultValue={selectedWard}
+                    />
           </div>
           <div className="grid gap-2">
             <Label htmlFor="email">Email</Label>
